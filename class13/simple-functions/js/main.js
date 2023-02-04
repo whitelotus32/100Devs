1c53a431bdcc7108be2536e9d701aa24bb3fa28d
Nicholas Dixon <naseer2152@gmail.com>
2023-02-03 18:13:28 -0800

    Add Two Numbers Function

--- class13/simple-functions/js/main.js
+++ class13/simple-functions/js/main.js
@@ -1,4 +1,9 @@
 //create a function that adds two numbers and alerts the sum
+function addTwoNumbers(num1, num2){
+    alert( num1 + num2 )
+}
+
+addTwoNumbers(2,3)
 
 //create a function that multiplys three numbers and console logs the product
 
