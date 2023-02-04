f4b5836f8169cc329d3bbaa1220ae46f1a6296e4
Nicholas Dixon <naseer2152@gmail.com>
2023-02-03 17:05:06 -0800

    Temp Converter

--- class13/temperature-converter/js/main.js
+++ class13/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
 //Write your pseduo code first! 
+
+// 0 -> 32
+document.querySelector('#yell').addEventListener('click', convert)
+
+function convert() {
+    // need the value that is in C 
+    let temp = document.querySelector('#cel').value
+    // convert from C to F 
+    temp = temp * 9/5 + 32
+    // show it
+    document.querySelector('#placeToYell').innerText = temp
+}
